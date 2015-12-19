2bbc34d97a15ab0912623cabe479925364db3d7a
rowlandjl <rowlandjl@vcu.edu>
2015-12-18 19:49:53 -0500

    LaunchAcademy/Ticket_Booth

---
+++ ticket_booth_part1.rb
@@ -0,0 +1,9 @@
+puts "Hello customer! Welcome to the park!"
+
+puts "How many adult tickets would you like?"
+adult = gets.chomp
+
+puts "How many child tickets would you like?"
+child = gets.chomp
+
+puts "Here are your " + adult + " adult tickets and " + child + " child tickets!"
---
+++ ticket_booth_part2.rb
@@ -0,0 +1,13 @@
+hot_dog = 1.27
+hamburger = 4.17
+funnel_cake = 3.79
+ice_cream_sandwich = 0.75
+
+puts "Two hamburgers and two ice cream sandwiches: "
+puts hamburger * 2 + ice_cream_sandwich * 2
+
+puts "Eight funnel cakes and a hot dog:"
+puts funnel_cake * 8 + hot_dog
+
+puts "Three of each item on the menu:"
+puts (hot_dog + hamburger + funnel_cake + ice_cream_sandwich) * 3
